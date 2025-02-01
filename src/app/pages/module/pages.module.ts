078173c61fbcf5f254e9b9eafe84ef6082349a9b
MalikUmair972 <mu501362@gmail.com>
2025-02-01 23:51:40 +0500

    pages module added

--- src/app/pages/module/pages.module.ts
+++ src/app/pages/module/pages.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    ServicesComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    HomeComponent
+    HomeComponent,
+    ServicesComponent
   ]
 })
 export class ModuleModule { }
