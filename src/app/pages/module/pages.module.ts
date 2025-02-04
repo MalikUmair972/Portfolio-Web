c9121879d701d0689d85e150257521753d97bd69
MalikUmair972 <mu501362@gmail.com>
2025-02-04 11:13:12 +0500

    Pages Module Updated

--- src/app/pages/module/pages.module.ts
+++ src/app/pages/module/pages.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { ServicesComponent } from './services/services.component';
+import { CoreFeatureComponent } from './core-feature/core-feature.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    ServicesComponent
+    ServicesComponent,
+    CoreFeatureComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HomeComponent,
-    ServicesComponent
+    ServicesComponent,
+    CoreFeatureComponent
   ]
 })
 export class ModuleModule { }
