847923ae163a6d3843cd182cb0bd6b219035963f
MalikUmair972 <mu501362@gmail.com>
2025-02-04 11:12:49 +0500

    Core feature in different component

---
+++ src/app/pages/module/core-feature/core-feature.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-core-feature',
+  templateUrl: './core-feature.component.html',
+  styleUrl: './core-feature.component.scss'
+})
+export class CoreFeatureComponent {
+
+
+  core = [
+    {
+      Image: 'assets/clean.png',
+      Title: 'Clean Code'
+    },
+    {
+      Image: 'assets/clean.png',
+      Title: 'Clean Code'
+    },
+    {
+      Image: 'assets/clean.png',
+      Title: 'Clean Code'
+    },
+    {
+      Image: 'assets/clean.png',
+      Title: 'Clean Code'
+    },
+    {
+      Image: 'assets/clean.png',
+      Title: 'Clean Code'
+    },
+    {
+      Image: 'assets/clean.png',
+      Title: 'Clean Code'
+    },
+  ]
+
+}
