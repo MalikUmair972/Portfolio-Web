0b76a77b33cd1e0bfcd077b7e54a92e9ffb6880c
MalikUmair972 <mu501362@gmail.com>
2025-02-24 11:14:07 +0500

    Component imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ModuleComponentModule } from './components/module-component/module-component.module'; 
 import { ModuleModule } from './pages/module/pages.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ModuleComponentModule ,
-    ModuleModule
+    ModuleModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
